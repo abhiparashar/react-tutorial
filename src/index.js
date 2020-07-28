d6d34b24eda1ef1f64cc7fd96e6dd38857eeedd5
abhi parashar <aparashar103@gmail.com>
2020-07-28 21:02:53 +0530

    practice

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDom from "react-dom"
+import './index.css'
+
+function People(){
+  return
+}
+
+
+ReactDom.render(<People/>, document.getElementById("root"));
